33357faca61b917990ff8faf6b2de4b71070c06d
kkauma <kylekauma@gmail.com>
2025-01-30 20:31:20 -0700

    Add initial photos go gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,16 +1,16 @@
-import { Projects } from "app/components/gallery";
+import Gallery from "app/components/gallery";
 
 export const metadata = {
-  title: "Projects",
-  description: "Things I've built.",
+  title: "Gallery",
+  description: "Places I've been.",
 };
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Gallery</h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <Projects />
+        <Gallery />
       </div>
     </section>
   );
